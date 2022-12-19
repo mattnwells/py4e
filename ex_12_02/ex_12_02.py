f0076f82588c3abc1fc40d5602fb31f72d6ccc2f
Matt Wells <matt.n.wells@gmail.com>
2022-12-18 18:46:31 -0800

    Add files via upload

---
+++ ex_12_02/ex_12_02.py
@@ -0,0 +1,30 @@
+"""
+Exercise 2: Change your socket program so that it counts the number of characters it has received and stops displaying any text after it has shown 3000 characters. The program should retrieve the entire document and count the total number of characters and display the count of the number of characters at the end of the document.
+"""
+
+import socket
+
+url=input("Enter URL: ")
+
+try:
+    host = url.split("/")[2]
+    mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    mysock.connect((host, 80))
+    cmd = ('GET '+ url+' HTTP/1.0\r\n\r\n').encode()
+    mysock.send(cmd)
+
+except:
+    print("Not a URL")
+    exit()
+
+count = 0
+
+while True:
+    data = mysock.recv(512)
+    for c in data:
+        count += 1
+    if len(data) < 1 or count > 3000:
+            break
+    
+    print(count)
+    mysock.close()
