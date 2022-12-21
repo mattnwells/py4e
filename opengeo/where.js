6f7d1310d268f74b51eea5f6b58ec66cf9c0ae5b
Matt Wells <matt.n.wells@gmail.com>
2022-12-21 07:36:55 -0800

    Add files via upload

---
+++ opengeo/where.js
@@ -0,0 +1,22 @@
+myData = [
+[50.065703299999996,19.918958667058632, 'AGH University of Science and Technology'],
+[52.2394966,21.015693129786936, 'Academy of Fine Arts in Warsaw'],
+[30.04281175,31.23658590813949, 'American University in Cairo'],
+[33.4213467,-111.93315832031976, 'Arizona State University'],
+[37.9425172,23.8706149, 'Athens Information Technology'],
+[17.547185,78.572791, 'BITS Pilani'],
+[6.88997525,3.7224790878605756, 'Babcock University'],
+[25.2662887,82.9927969, 'Banaras Hindu University'],
+[12.944172049999999,77.5091485894286, 'Bangalore University'],
+[31.55043405,-97.1102905567766, 'Baylor University'],
+[39.959732,116.35973698189176, 'Beijing Normal University (BNU)'],
+[53.83806745,27.476155257195504, 'Belarusian State University'],
+[45.779718,-111.177778, 'Belarusian State University'],
+[42.5009819,-89.0157777411969, 'Beloit College'],
+[35.95422,-90.45997, 'Beloit College'],
+[31.2631254,34.802238544584185, 'Ben Gurion University of the Negev'],
+[42.350189400000005,-71.10070118497421, 'Boston University'],
+[35.30059,-120.66004, 'California Polytechnic State University - San Luis Obispo'],
+[34.18191745,-117.32174391836504, 'California State University, San Bernardino'],
+[51.5213399,-0.1748934415893599, 'City of Westminster College']
+];
